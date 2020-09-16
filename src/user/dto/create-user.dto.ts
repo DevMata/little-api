0eb03197862df9851f11fc5f37020174ceb4e262
Antonio Mata <hildeamp@gmail.com>
2020-09-15 22:08:59 -0600

    feat: add validations to CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @IsEmail()
   email: string;
 }
